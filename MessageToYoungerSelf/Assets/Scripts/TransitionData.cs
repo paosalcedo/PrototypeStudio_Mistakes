c751731af6d8b33fb243bc1e8803177c0f8e5a3e
pao_s <paosalcedo@gmail.com>
2018-04-19 22:34:37 -0400

    Added rewired, etc.

---
+++ MessageToYoungerSelf/Assets/Scripts/TransitionData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionData
+{
+
+    public readonly Difficulty difficulty;
+    public readonly int score;
+    public readonly string difficultyName;
+
+    public TransitionData(Difficulty difficulty = null, string name = "", int score = 0)
+    {
+        this.difficulty = difficulty;
+        this.score = score;
+        this.difficultyName = name;
+    }
+
+}
